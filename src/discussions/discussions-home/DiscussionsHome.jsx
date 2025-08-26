65a3355552b67b6cb6b43607fcba0064a37b3852
Stanislav <ananasgo@gmail.com>
2025-08-26 09:33:10 +0300

    fix: [TEA-289] Custom fonts loading fix (#10)

--- src/discussions/discussions-home/DiscussionsHome.jsx
+++ src/discussions/discussions-home/DiscussionsHome.jsx
@@ -8,6 +8,7 @@ import {
   matchPath, Route, Routes, useLocation, useMatch,
 } from 'react-router-dom';
 
+import { FooterSlot } from '@edx/frontend-component-footer';
 import { LearningHeader as Header } from '@edx/frontend-component-header';
 
 import { Spinner } from '../../components';
@@ -27,7 +28,6 @@ import { selectPostEditorVisible } from '../posts/data/selectors';
 import { isCourseStatusValid } from '../utils';
 import useFeedbackWrapper from './FeedbackWrapper';
 
-const FooterSlot = lazy(() => import('@edx/frontend-component-footer').then(module => ({ default: module.FooterSlot })));
 const PostActionsBar = lazy(() => import('../posts/post-actions-bar/PostActionsBar'));
 const CourseTabsNavigation = lazy(() => import('../../components/NavigationBar/CourseTabsNavigation'));
 const LegacyBreadcrumbMenu = lazy(() => import('../navigation/breadcrumb-menu/LegacyBreadcrumbMenu'));
